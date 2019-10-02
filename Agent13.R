7515533f818377d67f83af672c23e8539290428c
jasperendlich <36659326+jasperendlich@users.noreply.github.com>
2019-10-02 09:30:35 +0200

    Add files via upload

--- Agent13.R
+++ Agent13.R
@@ -42,22 +42,35 @@ Agent <- R6Class(
     },
     
     get_bid = function() {
+      #There are agents in there that only defect, and have "Lemon!" as a greeting
+      #This is the way to filter them out
       if (self$opponent_greeting == "Lemon!") {
         self$bid <- "defect"
       }
+      
       else {
+        #Tit-for-tat strategy is based on being cooperative the first round
         if (self$round == 1) {
           self$bid <- "cooperate"
         }
+        
+        #After that we take the last bid of our opponent 
+        #(the last bid against anyone, right now)
         else if (self$round > 1) {
+          #We're searching for all the rows in which our opponent played
           opp_id_one <- which(book$id1 == self$opponent_id)
           opp_id_two <- which(book$id2 == self$opponent_id)
           
+          #For the max function to work, both columns need one occurence
+          #Otherwise we cooperate
           if (length(opp_id_one) >= 1 && length(opp_id_two) >= 1) {
+            #Check which row contains the last bid of our opponent
             if (max(opp_id_one) > max(opp_id_two)) {
+              #Get the value in col 'bid1' on that last row, and copy it as our bid
               self$bid <- as.vector(book$bid1[max(opp_id_one)])
             }
             else {
+              #Get the value in col 'bid2' on that last row, and copy it as our bid
               self$bid <- as.vector(book$bid2[max(opp_id_one)])
             }
           }
